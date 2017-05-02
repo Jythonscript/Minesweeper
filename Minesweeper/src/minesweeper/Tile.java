748d1aede9a25024f5a980baa2c9ea7d35cb8091
90302927 <90302927@hs-06301-macair.ad.edenpr.org>
2017-05-02 14:20:26 -0500

    Added comments and cleaned up the tile class.

--- Minesweeper/src/minesweeper/Tile.java
+++ Minesweeper/src/minesweeper/Tile.java
@@ -1,21 +1,43 @@
-//Individual
-
 package minesweeper;
 
+//determines the properties that the tiles can have
 public class Tile {
 	
-	
+	//fields
 	private boolean isHidden;
-	
 	private boolean isMine;
-	
 	private int num;
 	
 
-	public Tile(boolean hidden, boolean Mine, int num){
-		isHidden = hidden;
-		isMine = Mine;
-		num = num;
-		
+	//default constructor
+	public Tile(boolean h, boolean m, int n){
+		this.isHidden = h;
+		this.isMine = m;
+		this.num = n;
 	}
+	
+	//constructor for before the number is known
+	public Tile(boolean h, boolean m) {
+		this.isHidden = h;
+		this.isMine = m;
+		this.num = 0;
+	}
+	
+	//un-hides a tile
+	public void show() {
+		this.isHidden = false;
+	}
+	
+	//returns if the tile is hidden
+	public boolean isHidden() 
+	{return this.isHidden;}
+	
+	//returns if the tile is a mine
+	public boolean isMine() 
+	{return this.isMine;}
+	
+	//returns the number on the tile
+	public int num() 
+	{return this.num;}
+	
 }
