9ee0232482c7aa0b0d297f28a9f8c42a07b44308
90302927 <90302927@hs-06301-macair.ad.edenpr.org>
2017-05-03 11:24:55 -0500

    Enabled double buffering in Screen.

--- Minesweeper/src/minesweeper/Runner.java
+++ Minesweeper/src/minesweeper/Runner.java
@@ -6,9 +6,12 @@ public class Runner {
 
 	public static void main(String[] args) {
 		
-//		Screen scr = new Screen();
-		Mines field = new Mines();
-		field.printMineField();
+		Screen scr = new Screen();
+		Thread t1 = new Thread(scr);
+		t1.start();
+		
+//		Mines field = new Mines();
+//		field.printMineField();
 			
 	}
 	
--- Minesweeper/src/minesweeper/Screen.java
+++ Minesweeper/src/minesweeper/Screen.java
@@ -1,12 +1,20 @@
 //Jframe that displays 
 package minesweeper;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 
 import javax.swing.JFrame;
 
 public class Screen extends JFrame implements Runnable{
 
+	private Image dbImage;
+	private Graphics dbg;
+	
+	private int screenX = 800;
+	private int screenY = 500;
+	
 	public void run() {
 		
 		try {
@@ -19,21 +27,32 @@ public class Screen extends JFrame implements Runnable{
 			
 		}
 		catch(Exception e) {
-			
 			System.out.println("Error in thread: " + e.getMessage());
-			
 		}
 		
 	}
 	
+	//printing stuff to the screen
+	public void paintComponent(Graphics g) {
+		
+		g.drawString("Test string", 100, 100);
+		repaint();
+		
+	}
+	
+	//double buffering
 	public void paint(Graphics g) {
 		
-		
+		dbImage = createImage(getWidth(), getHeight());
+		dbg = dbImage.getGraphics();
+		paintComponent(dbg);
+		g.drawImage(dbImage, 0, 0, this);
 		
 	}
 	
 	public Screen() {
 		
+		setTitle("Minesweeper");
 		setVisible(true);
 		setSize(800, 500);
 		setResizable(false);
