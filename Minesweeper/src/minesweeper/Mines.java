7298c6c4f5744a8d5681aea4936b58543ef55f48
90301472 <90301472@hs-06760-macair.ad.edenpr.org>
2017-05-03 11:13:04 -0500

    Fixed getters

--- Minesweeper/src/minesweeper/Mines.java
+++ Minesweeper/src/minesweeper/Mines.java
@@ -8,12 +8,12 @@ public class Mines {
 	//0 = open 9 == mine, 1= 1 mine nearby, 2 = 2 mines nearby ...
 	//Randomly assigns the tiles in a 2d grid a value of bomb or not.	
 	public Mines(){
-		
+
 		//creates a minesweeper board 20x20
 		tiles = new Tile[20][20];
-		
+
 		for(int r = 0;r<20;r++){
-			
+
 			for(int c = 0;c<20;c++){
 				//Randomly assigns tile as mine or not mine
 				int ran = ((int)(Math.random() * 4 + 1));
@@ -25,24 +25,24 @@ public class Mines {
 				}
 			}
 		}
-		
-		
+
+
 	}
 	//Prints minefield of bomb status 
 	public void printMineField(){
-		
+
 		for(int r = 0;r<20;r++){
-			
+
 			for(int c = 0;c<20;c++){
-			
-			System.out.print(tiles[r][c].isMine() + "\t");
-				
+
+				System.out.print(tiles[r][c].getIsMine() + "\t");
+
 			}
 			System.out.println();
+		}
+
+
 	}
 
-	
+
 }
-	
-	
-	}
